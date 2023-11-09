027bfbf29ef75785a9920bd117e276f7a1ded0f3
kahfiaulia <kahfiaulia39@gmail.com>
2023-11-09 10:47:14 +0700

    initial commit

---
+++ src/appinventor/ai_kahfiaulia39/SpeechRecog4611418009/Screen1.scm
@@ -0,0 +1,4 @@
+#|
+$JSON
+{"authURL":["ai2.appinventor.mit.edu"],"YaVersion":"208","Source":"Form","Properties":{"$Name":"Screen1","$Type":"Form","$Version":"27","AppName":"SpeechRecog4611418009","Title":"Screen1","Uuid":"0","$Components":[{"$Name":"Rekam","$Type":"Button","$Version":"6","Text":"Rekam","Uuid":"330747734"},{"$Name":"Hasil","$Type":"Label","$Version":"5","Text":"Hasil","Uuid":"1921062650"},{"$Name":"SpeechRecognizer1","$Type":"SpeechRecognizer","$Version":"2","Uuid":"-620293180"}]}}
+|#
